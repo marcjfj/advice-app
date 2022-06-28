3865fcee8ba8e04aa179c892d145689735ae35ee
marcjfj <marcjfj@gmail.com>
2022-06-27 23:15:23 -0500

    clean up config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      'light': 'hsl(193, 38%, 86%)',
-      'accent': 'hsl(150, 100%, 66%)',
+      light: 'hsl(193, 38%, 86%)',
+      accent: 'hsl(150, 100%, 66%)',
       'gray-light': 'hsl(217, 19%, 38%)',
       'gray-dark': 'hsl(217, 19%, 24%)',
       'blue-dark': 'hsl(218, 23%, 16%)',
     },
     fontFamily: {
-      'sans': ['Manrope', 'sans-serif'],
+      sans: ['Manrope', 'sans-serif'],
     },
     extend: {
       fontSize: {
         '2xl': `${28 / 16}rem`,
       },
       letterSpacing: {
-        'omg': '.35em',
-      }
+        omg: '.35em',
+      },
     },
   },
-  plugins: [],
-}
+};
